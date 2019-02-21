c1247e88c06ddc87921038aedb270e91c882e911
maxcan7 <maxcan7@gmail.com>
2019-02-21 15:54:37 -0500

    better comments in datamigration.py

--- datamigration.py
+++ datamigration.py
@@ -23,9 +23,17 @@ def unzip_data(datapath=sys.argv[1]):
 
 # Get keylist for orders table
 def orders_keylist(d):
+    # The json data is a dictionary with key 'orders' and value being
+    # a dictionary for that order. We need to extract the keys from
+    # one of those orders.
     orderkeys = [key for key in d['orders'][0].keys() if key != 'line_items']
+    # Likewise, we need to extract the values of each order for each key,
+    # besides line_items, since that is also a value that is a separate
+    # dictionary, and will go into its own table.
     idx = 0
+    # For each order
     for order in d['orders']:
+        # Get values for each key besides line_items
         if idx == 0:
             ordervals = [[val for val in d['orders'][idx].values()
                          if d['orders'][idx].keys() != 'line_items']]
@@ -41,6 +49,7 @@ def orders_keylist(d):
 
 # Get keylist for line_items table
 def lineitems_keylist(d):
+    # Essentially the same logic as in the comments for def orders_kelist
     lineitems_keys = ['order_id'] + \
                 [key for key in d['orders'][0]['line_items'][0].keys()]
     idx = 0
@@ -92,7 +101,9 @@ def insert_orders(tablename, keys, values):
             conn = psycopg2.connect(**params)
             # create a new cursor
             cur = conn.cursor()
-            # execute the INSERT statement
+            # execute the INSERT statement with the inputted
+            # tablename, the ith key, and all order values
+            # for the ith key
             cur.executemany(sql, [tablename, keys[i], ival])
             # commit the changes to the database
             conn.commit()
