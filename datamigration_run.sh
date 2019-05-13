e742655fca2222ea0d5a0f7f74c516ae1d17e44f
maxcan7 <maxcan7@gmail.com>
2019-05-13 15:02:00 -0400

    .gitignore fix

--- datamigration_run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mainpath='C:/Users/maxca/Documents/GitHub/codingchallenge_datamigration/'
-datapath='data/data.zip'
-datapath=$mainpath$datapath
-config='C:/Users/maxca/Documents/GitHub/codingchallenge_datamigration/database/datamigration_db.ini'
-./src/datamigration.py $datapath $config
--- datamigration_tablecreate.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-config='C:/Users/maxca/Documents/GitHub/codingchallenge_datamigration/database/datamigration_db.ini'
-./database/datamigration_createtable.py $config
