8819674e229b0d4222b8471e93fb23143209a245
maxcan7 <maxcan7@gmail.com>
2019-05-13 14:58:03 -0400

    now opens each extracted file, preprocesses, and inserts into the tables in the db iteratively in a loop

--- datamigration_dbcreate_example.sh
+++ datamigration_dbcreate_example.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-postgresuser='postgres'
-postgreshost='localhost'
-db_password='****'
-./src/datamigration.py $postgresuser $postgreshost $db_password
+defaultdb='postgres'
+port=5432
+user='postgres'
+password='****'
+host='localhost'
+./database/datamigration_createdb.py $defaultdb $port $user $password $host
---
+++ datamigration_tablecreate_example.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+config='inifile path'
+./database/datamigration_createtable.py $config
